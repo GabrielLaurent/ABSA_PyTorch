39e3c8ef7588706bc3b900a2c1702e7fc373337b
GabrielLaurent <gabriellaurent.embellish948@slmail.me>
2022-10-09 14:58:40 +0800

    Implement: Add Checkpointing\ (#13)

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -1,60 +1,78 @@
 import torch
 import torch.nn as nn
-from sklearn.metrics import accuracy_score, f1_score
-import logging
+import os
 
 class Trainer:
-    def __init__(self, model, optimizer, criterion, device):
+    def __init__(self, model, optimizer, criterion, config, device, train_dataloader, val_dataloader):
         self.model = model
         self.optimizer = optimizer
         self.criterion = criterion
+        self.config = config
         self.device = device
-        self.logger = logging.getLogger(__name__)
+        self.train_dataloader = train_dataloader
+        self.val_dataloader = val_dataloader
+        self.best_val_loss = float('inf')
+        self.checkpoint_dir = config['training']['checkpoint_dir'] # Read checkpoint directory from config
 
-    def train_epoch(self, data_loader):
+        # Create checkpoint directory if it doesn't exist
+        if not os.path.exists(self.checkpoint_dir):
+            os.makedirs(self.checkpoint_dir)
+
+    def train_epoch(self):
         self.model.train()
         total_loss = 0
-        for batch in data_loader:
+
+        for batch in self.train_dataloader:
             inputs = batch['text'].to(self.device)
+            aspects = batch['aspect'].to(self.device)
             labels = batch['label'].to(self.device)
+
             self.optimizer.zero_grad()
-            outputs = self.model(inputs)
+            outputs = self.model(inputs, aspects)
             loss = self.criterion(outputs, labels)
             loss.backward()
             self.optimizer.step()
+
             total_loss += loss.item()
-        return total_loss / len(data_loader)
 
-    def evaluate(self, data_loader):
+        return total_loss / len(self.train_dataloader)
+
+    def validate_epoch(self):
         self.model.eval()
-        true_labels = []
-        predicted_labels = []
         total_loss = 0
+
         with torch.no_grad():
-            for batch in data_loader:
+            for batch in self.val_dataloader:
                 inputs = batch['text'].to(self.device)
+                aspects = batch['aspect'].to(self.device)
                 labels = batch['label'].to(self.device)
-                outputs = self.model(inputs)
+
+                outputs = self.model(inputs, aspects)
                 loss = self.criterion(outputs, labels)
+
                 total_loss += loss.item()
-                _, predicted = torch.max(outputs, 1)
-                true_labels.extend(labels.cpu().numpy())
-                predicted_labels.extend(predicted.cpu().numpy())
 
-        accuracy = accuracy_score(true_labels, predicted_labels)
-        f1 = f1_score(true_labels, predicted_labels, average='macro') # or 'binary', 'micro', 'weighted'
-        avg_loss = total_loss / len(data_loader)
+        return total_loss / len(self.val_dataloader)
 
-        self.logger.info(f"Evaluation - Loss: {avg_loss:.4f}, Accuracy: {accuracy:.4f}, F1: {f1:.4f}")
-        return avg_loss, accuracy, f1
+    def train(self):
+        num_epochs = self.config['training']['epochs']
+        for epoch in range(num_epochs):
+            train_loss = self.train_epoch()
+            val_loss = self.validate_epoch()
 
-    def predict(self, data_loader):
-      self.model.eval()
-      predicted_labels = []
-      with torch.no_grad():
-        for batch in data_loader:
-          inputs = batch['text'].to(self.device)
-          outputs = self.model(inputs)
-          _, predicted = torch.max(outputs, 1)
-          predicted_labels.extend(predicted.cpu().numpy())
-      return predicted_labels
+            print(f'Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}')
+
+            # Save checkpoint if validation loss improved
+            if val_loss < self.best_val_loss:
+                self.best_val_loss = val_loss
+                self.save_checkpoint(epoch, val_loss)
+                print(f'\t--> Checkpoint saved - val_loss: {val_loss:.4f}')
+
+    def save_checkpoint(self, epoch, val_loss):
+        checkpoint_path = os.path.join(self.checkpoint_dir, 'best_model.pth') # Save as best_model.pth by default, overwriting previous best
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'val_loss': val_loss,
+        }, checkpoint_path)
