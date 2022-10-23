ed94d1d9ed7359a231bccb01326a730435444a02
GabrielLaurent <gabriellaurent.embellish948@slmail.me>
2022-10-23 08:25:26 +0800

    Implement: Implement Pre-trained Embeddings\ (#10)

--- src/data/vocabulary.py
+++ src/data/vocabulary.py
@@ -1,24 +1,52 @@
+import torch
+import numpy as np
+
 class Vocabulary:
-    def __init__(self, tokens=None):
-        self.token_to_index_map = {'<pad>': 0, '<unk>': 1}
-        self.index_to_token_map = {0: '<pad>', 1: '<unk>'}
-        self.next_index = len(self.token_to_index_map)
+    def __init__(self, data, config):
+        self.word2idx = {'<pad>': 0, '<unk>': 1}
+        self.idx2word = {0: '<pad>', 1: '<unk>'}
+        self.word_counts = {}
+        self.config = config
+        self.embedding_matrix = None
+        self.build_vocabulary(data)
 
-        if tokens is not None:
-            self.build_vocabulary(tokens)
 
-    def build_vocabulary(self, tokens):
-        for token in tokens:
-            if token not in self.token_to_index_map:
-                self.token_to_index_map[token] = self.next_index
-                self.index_to_token_map[self.next_index] = token
-                self.next_index += 1
+    def build_vocabulary(self, data):
+        for sentence in data:
+            for word in sentence.split():
+                if word not in self.word_counts:
+                    self.word_counts[word] = 0
+                self.word_counts[word] += 1
 
-    def token_to_index(self, token):
-        return self.token_to_index_map.get(token, 1)  # Return <unk> index if token is not in vocab
+        for word, count in self.word_counts.items():
+            if word not in self.word2idx:
+                index = len(self.word2idx)
+                self.word2idx[word] = index
+                self.idx2word[index] = word
 
-    def index_to_token(self, index):
-        return self.index_to_token_map.get(index, '<unk>')
+
+    def load_pretrained_embeddings(self):
+        print("Loading pretrained embeddings from {}".format(self.config['pretrained_embeddings_path']))
+        embeddings_index = {}
+        with open(self.config['pretrained_embeddings_path'], encoding='utf8') as f:
+            for line in f:
+                values = line.split()
+                word = values[0]
+                coefs = np.asarray(values[1:], dtype='float32')
+                embeddings_index[word] = coefs
+
+        print('Found %s word vectors.' % len(embeddings_index))
+
+        embedding_matrix = np.zeros((len(self.word2idx), self.config['embedding_dim']))
+        for word, i in self.word2idx.items():
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                # words not found in embedding index will be all-zeros.
+                embedding_matrix[i] = embedding_vector
+
+        self.embedding_matrix = torch.tensor(embedding_matrix, dtype=torch.float)
+        print("Pretrained embeddings loaded")
+
 
     def __len__(self):
-        return len(self.token_to_index_map)
+        return len(self.word2idx)
--- src/models/model.py
+++ src/models/model.py
@@ -1,50 +1,31 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-class ABSAModel(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, embedding_weights):
-        super(ABSAModel, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.embedding.weight = nn.Parameter(torch.tensor(embedding_weights, dtype=torch.float32))
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True, bidirectional=True)
-        self.fc = nn.Linear(hidden_dim * 2, output_dim) # Bi-directional LSTM so * 2
+class BaseModel(nn.Module):
+    def __init__(self, vocab, config):
+        super(BaseModel, self).__init__()
+        self.embedding_dim = config['embedding_dim']
+        self.hidden_dim = config['hidden_dim']
+        self.num_layers = config['num_layers']
+        self.dropout = config['dropout']
+        self.vocab = vocab
 
-    def forward(self, text, aspect, text_lengths, aspect_lengths):
-        # text: [batch size, seq len]
-        # aspect: [batch size, aspect len]
-        # text_lengths: [batch size]
-        # aspect_lengths: [batch size]
+        self.embedding = nn.Embedding(len(vocab), self.embedding_dim, padding_idx=0)
 
-        # embedded: [batch size, seq len, emb dim]
-        embedded_text = self.embedding(text)
-        embedded_aspect = self.embedding(aspect)
+        # Load pre-trained embeddings if available
+        if vocab.embedding_matrix is not None:
+            self.embedding.weight.data.copy_(vocab.embedding_matrix)
+            if not config['fine_tune_embeddings']:
+                self.embedding.weight.requires_grad = False
 
-        # Pack padded sequences
-        packed_embedded_text = nn.utils.rnn.pack_padded_sequence(embedded_text, text_lengths.cpu(), batch_first=True, enforce_sorted=False)
-        packed_embedded_aspect = nn.utils.rnn.pack_padded_sequence(embedded_aspect, aspect_lengths.cpu(), batch_first=True, enforce_sorted=False)
+        self.lstm = nn.LSTM(self.embedding_dim, self.hidden_dim, num_layers=self.num_layers, dropout=self.dropout, batch_first=True)
+        self.dropout_layer = nn.Dropout(self.dropout)
 
-        # lstm_out: [batch size, seq len, hid dim * 2]
-        lstm_out_text, (hidden_text, cell_text) = self.lstm(packed_embedded_text)
-        lstm_out_aspect, (hidden_aspect, cell_aspect) = self.lstm(packed_embedded_aspect)
+        self.fc = nn.Linear(self.hidden_dim, len(config['aspect_categories']))
 
-        # Unpack
-lstm_out_text, _ = nn.utils.rnn.pad_packed_sequence(lstm_out_text, batch_first=True)
-lstm_out_aspect, _ = nn.utils.rnn.pad_packed_sequence(lstm_out_aspect, batch_first=True)
-
-        # Average the hidden states for aspect representation
-aspect_representation = torch.sum(lstm_out_aspect, dim=1) / aspect_lengths.unsqueeze(1).float().to(lstm_out_aspect.device)
-
-        # Concatenate text and aspect representations (you might want to implement attention here)
-        # Here, simply using the last hidden state from the text LSTM
-        # last_hidden_text = hidden_text[-1,:,:]
-        # concatenated = torch.cat((last_hidden_text, aspect_representation), dim=1) # dimension error due to bidirectional lstm
-        # Alternatively, take the last hidden state from both directions:
-        last_hidden_forward = hidden_text[0,:,:]  # Forward direction
-        last_hidden_backward = hidden_text[1,:,:] # Backward direction
-        concatenated = torch.cat((last_hidden_forward, last_hidden_backward, aspect_representation), dim=1)
-        concatenated = torch.relu(concatenated) #add relu to stabilize
-        # Fully connected layer
-        output = self.fc(concatenated)
-
+    def forward(self, text):
+        embedded = self.embedding(text)
+        output, (hidden, cell) = self.lstm(embedded)
+        output = self.dropout_layer(output[:, -1, :])  # Use the last hidden state
+        output = self.fc(output)
         return output
