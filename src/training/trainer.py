512d8c40367ceca215819d6c09880dd5675d9555
GabrielLaurent <gabriellaurent.embellish948@slmail.me>
2022-11-11 00:55:47 +0800

    Implement: Implement Training Epoch\ (#6)

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -1,36 +1,62 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
+from torch.optim import AdamW
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 class Trainer:
-    def __init__(self, model, config, device):
+    def __init__(self, model: nn.Module, train_dataloader: DataLoader, val_dataloader: DataLoader, config: dict):
         self.model = model
+        self.train_dataloader = train_dataloader
+        self.val_dataloader = val_dataloader
         self.config = config
-        self.device = device
-        self.loss_function = nn.CrossEntropyLoss()
-        self.optimizer = optim.Adam(model.parameters(), lr=config['learning_rate'])
+        self.device = config['device']
+        self.model.to(self.device)
+        self.optimizer = AdamW(self.model.parameters(), lr=config['learning_rate'], weight_decay=config['weight_decay'])
 
-    def train_epoch(self, dataloader):
+    def train_epoch(self, epoch_num: int):
         self.model.train()
         total_loss = 0
-        for batch in dataloader:
-            inputs = batch['text'].to(self.device)
-            aspects = batch['aspect'].to(self.device)
-            targets = batch['sentiment'].to(self.device)
-
+        progress_bar = tqdm(enumerate(self.train_dataloader), total=len(self.train_dataloader), desc=f"Epoch {epoch_num}")
+        for idx, batch in progress_bar:
             self.optimizer.zero_grad()
 
-            outputs = self.model(inputs, aspects)
-            loss = self.loss_function(outputs, targets)
+            input_ids = batch['input_ids'].to(self.device)
+            attention_mask = batch['attention_mask'].to(self.device)
+            labels = batch['labels'].to(self.device)
+
+            outputs = self.model(input_ids, attention_mask)
+            loss_fn = nn.CrossEntropyLoss()
+            loss = loss_fn(outputs, labels)
 
             loss.backward()
             self.optimizer.step()
 
             total_loss += loss.item()
 
-        return total_loss / len(dataloader)
+            progress_bar.set_postfix({"loss": total_loss / (idx + 1)})
 
-    def evaluate(self, dataloader):
+        return total_loss / len(self.train_dataloader)
+
+    def evaluate(self):
         self.model.eval()
-        # Add evaluation logic later
-        pass
+        total_loss = 0
+        with torch.no_grad():
+            for batch in self.val_dataloader:
+                input_ids = batch['input_ids'].to(self.device)
+                attention_mask = batch['attention_mask'].to(self.device)
+                labels = batch['labels'].to(self.device)
+
+                outputs = self.model(input_ids, attention_mask)
+                loss_fn = nn.CrossEntropyLoss()
+                loss = loss_fn(outputs, labels)
+
+                total_loss += loss.item()
+
+        return total_loss / len(self.val_dataloader)
+
+    def train(self):
+        for epoch in range(self.config['epochs']):
+            train_loss = self.train_epoch(epoch + 1)
+            val_loss = self.evaluate()
+            print(f"Epoch {epoch + 1}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
