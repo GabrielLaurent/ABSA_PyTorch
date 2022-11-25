41d4029d792de6431f7f327e5c2d42fb6e097465
GabrielLaurent <gabriellaurent.embellish948@slmail.me>
2022-11-25 10:41:32 +0800

    Implement: Define Initial Model Architecture\ (#3)

--- src/models/model.py
+++ src/models/model.py
@@ -1,18 +1,31 @@
-# src/models/model.py
-
 import torch
 import torch.nn as nn
 
-class ABSAModel(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim):
-        super(ABSAModel, self).__init__()
+class BaseModel(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, embedding_weights=None):
+        super(BaseModel, self).__init__()
+        self.embedding_dim = embedding_dim
+
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        if embedding_weights is not None:
+            self.embedding.weight.data.copy_(embedding_weights)
+
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
         self.fc = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, text):
+        # text: [batch size, seq len]
         embedded = self.embedding(text)
+        # embedded: [batch size, seq len, embedding dim]
+        
         output, (hidden, cell) = self.lstm(embedded)
-        # Use the last hidden state as the sentence representation
-        output = self.fc(hidden[-1])
+        # output: [batch size, seq len, hidden dim]
+        # hidden: [1, batch size, hidden dim]
+
+        # We only use the last hidden state
+        hidden = hidden.squeeze(0)
+
+        output = self.fc(hidden)
+        # output: [batch size, output dim]
+
         return output
